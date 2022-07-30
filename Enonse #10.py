a2f4571c5d588ead711d5f6c8685a1d0c22d43dc
Peary74 <maussat@outlook.com>
2022-07-30 16:06:56 -0400

    add functionnality

--- Enonse #10.py
+++ Enonse #10.py
@@ -3,9 +3,6 @@ from os import remove
 
 
 Note= "web-insecure;34829sjdfnsj32984madsdkj"
-
 note=Note.split(";")
-print(note)
 Note=note[1]
-
 print(Note)
--- Enonse #16.py
+++ Enonse #16.py
@@ -1,5 +1,31 @@
 
 karakte="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 kod=input("Antre kod lan:")
-for i,el in enumerate (kod):
+rezilta=""
+output=""
+bonKod=kod.split(" ")
+# print(bonKod)
+for i,el in enumerate (bonKod):
+    kodd= ' '.join(el)
+    kodd=kodd.replace(" ","")
+    if kodd[0] == ">":
+        if(len(kodd)==3):
+            chif=int(kodd[1])
+        elif (len(kodd)==4):
+            chif=int(kodd[1:3])
+        let=kodd[-1]
+        endeks=karakte.index(let)
+        rezilta=karakte[endeks+chif]
     
+    elif kodd[0] == "<":
+        if(len(kodd)==3):
+            chif=int(kodd[1])
+        elif (len(kodd)==4):
+            chif=int(kodd[1:3])
+        let=kodd[-1]
+        endeks=karakte.index(let)
+        rezilta=karakte[endeks-chif]
+        
+    output+=rezilta 
+        
+print(output)
